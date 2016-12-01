1674596e81c06662880d7f1928b75a2b308509ba
Mitch Loar <mitchloarweb@gmail.com>
2016-12-01 00:30:12 -0700

    attempted sounds - played with CSS

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,48 +1,52 @@
 (function () {
-  
+
   const SESSIONTIME = 1500
   const BREAKTIME = 300
   const LONGBREAKTIME = 1800
-  
 
   function HomeCtrl($interval, $firebaseArray) {
     $ctrl = this;
     $ctrl.currentTime = SESSIONTIME;
     var timer;
+    var dingDong = new buzz.sound("assets/sounds/ding.mp3", {
+      preload: true
+    });
+
+
     $ctrl.status = "session"
-  
+
     $ctrl.completedSessions = 0
     $ctrl.startBtn = "Start";
 
     $ctrl.startTimer = function () {
-      
-      if(timer){
+      console.log('started')
+      if (timer) {
         $interval.cancel(timer)
       }
-      
+
       timer = $interval(function () {
-        if($ctrl.currentTime-- == 0){
-               
-          
+        if ($ctrl.currentTime-- == 0) {
+
+
           $interval.cancel(timer)
-            
-          if($ctrl.status == "session"){
+
+          if ($ctrl.status == "session") {
             $ctrl.completedSessions++;
-            
-            
-            if($ctrl.completedSessions == 4 ){
-              $ctrl.currentTime = LONGBREAKTIME;  
+
+
+            if ($ctrl.completedSessions == 4) {
+              $ctrl.currentTime = LONGBREAKTIME;
               $ctrl.status = "break"
-            }else{
-              $ctrl.currentTime = BREAKTIME;  
+            } else {
+              $ctrl.currentTime = BREAKTIME;
               $ctrl.status = "break"
             }
-          }else{
+          } else {
             $ctrl.currentTime = SESSIONTIME;
-            $ctrl.status = "session"    
-            
+            $ctrl.status = "session"
+
           }
-          
+
         }
       }, 1000)
     }
@@ -50,24 +54,43 @@
     $ctrl.stopBtn = "Stop";
 
     $ctrl.stopTimer = function () {
+      console.log('stopped')
       $interval.cancel(timer)
     }
-    
+
     $ctrl.resetBtn = "Reset";
-    
+
     $ctrl.resetTimer = function () {
-        console.log("reset!"); 
+      console.log("reset");
       $interval.cancel(timer)
       $ctrl.currentTime = SESSIONTIME
     }
-    //Begin Firebase Stuff
-    
+
+    //    Test out sounds to make sure file is loaded correctly
+    $ctrl.testBtn = "testingSound";
+
+    $ctrl.testSound = function () {
+      console.log('testing works!');
+      dingDong.play();
+    }
+
+
+    //    Play a ding 
+    //    $ctrl.$watch('currentTime', function () {
+    //      if ($ctrl.currentTime == 0) {
+    //        dingDong.play();
+    //      }
+    //    });
+
+
+    //  <----- Begin Firebase Stuff ------>
+
     $ctrl.tasks = $firebaseArray(firebase.database().ref().child("/tasks"));
     $ctrl.newTask = {}
-    
+
     $ctrl.addTask = () => {
       $ctrl.tasks.$add($ctrl.newTask);
-      
+
       $ctrl.newTask = {};
     }
   }
